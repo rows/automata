5e4e6ac88c605f2496f1712c63f161cf522ecd54
canastro <ricardocanastro@gmail.com>
2022-02-08 22:30:04 +0000

    refactor: add trigger method to transition

--- lib/src/transition_definition.dart
+++ lib/src/transition_definition.dart
@@ -1,8 +1,10 @@
-import 'package:state_machine/src/types.dart';
+import 'package:state_machine/src/state_node.dart';
+
+import '../state_machine.dart';
 
 class TransitionDefinition<E extends Event> {
   /// The state this transition is attached to.
-  final StateNode<State> fromStateNode;
+  final StateNodeDefinition<State> fromStateNode;
 
   final GuardCondition<E>? condition;
   final List<Action<E>>? actions;
@@ -20,7 +22,7 @@ class OnTransitionDefinition<S extends State, E extends Event,
   Type toState;
 
   OnTransitionDefinition({
-    required StateNode fromState,
+    required StateNodeDefinition fromState,
     GuardCondition<E>? condition,
     required this.toState,
     List<Action<E>>? actions,
@@ -29,4 +31,131 @@ class OnTransitionDefinition<S extends State, E extends Event,
           condition: condition,
           actions: actions,
         );
+
+  StateNodeDefinition? findLeaf(Type state, StateNodeDefinition node) {
+    final currentNode = node;
+    for (final key in currentNode.childNodes.keys) {
+      final childNode = currentNode.childNodes[key];
+      if (key == state) {
+        return childNode;
+      }
+
+      if (childNode != null) {
+        final res = findLeaf(state, childNode);
+        if (res != null) {
+          return res;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  /// Walks up the tree looking for an ancestor that is common
+  /// to the [fromAncestors] and [toAncestors] paths.
+  ///
+  /// If no common ancestor is found then null is returned;
+  StateNodeDefinition findCommonAncestor(
+    StateNodeDefinition from,
+    StateNodeDefinition to,
+  ) {
+    final toAncestorSet = to.path.toSet();
+    final fromPath = [from, ...from.path.reversed];
+
+    for (final ancestor in fromPath) {
+      if (toAncestorSet.contains(ancestor)) {
+        return ancestor;
+      }
+    }
+
+    throw Exception('from and to nodes do not share a common rootnode');
+  }
+
+  List<StateNodeDefinition> _getExitNodes(
+    StateMachineValue value,
+    StateNodeDefinition from,
+    StateNodeDefinition to,
+  ) {
+    final nodes = from.path
+        .where(
+          (element) => !to.path.contains(element),
+        )
+        .toList();
+
+    for (final node in value.activeLeafStates()) {
+      if (node.path.contains(to)) {
+        nodes.add(node);
+      }
+    }
+
+    return [...nodes, from];
+  }
+
+  List<StateNodeDefinition> _getIntialEnterNodes(StateNodeDefinition node) {
+    var result = <StateNodeDefinition>[];
+
+    if (node.stateNodeType == StateNodeType.parallel) {
+      for (final childNode in node.childNodes.values) {
+        result.add(childNode);
+        result.addAll(_getIntialEnterNodes(childNode));
+      }
+    } else if (node.initialStateNode != null) {
+      result.add(node.initialStateNode!);
+    }
+
+    return result;
+  }
+
+  List<StateNodeDefinition> _getEnterNodes(
+    StateNodeDefinition from,
+    StateNodeDefinition to,
+  ) {
+    final nodes = to.path.where(
+      (element) => !from.path.contains(element),
+    );
+
+    return [...nodes, to, ..._getIntialEnterNodes(to)];
+  }
+
+  StateMachineValue trigger(StateMachineValue value, E e) {
+    final fromLeaf = fromStateNode;
+    final toLeaf = findLeaf(toState, fromLeaf.rootNode);
+
+    if (toLeaf == null) {
+      throw Exception('destination leaf node not found');
+    }
+
+    // find common ancestor between these nodes
+    final ancestor = findCommonAncestor(fromLeaf, toLeaf);
+
+    // trigger all on exits based on common ancestor
+    final exitNodes = _getExitNodes(value, fromLeaf, toLeaf);
+    for (final node in exitNodes) {
+      node.callExit(e);
+    }
+
+    // trigger all actions
+    if (actions != null && actions!.isNotEmpty) {
+      for (final action in actions!) {
+        action(e);
+      }
+    }
+
+    // trigger all on enters based on common ancestor
+    final enterNodes = _getEnterNodes(fromLeaf, toLeaf);
+    for (final node in enterNodes) {
+      node.callEnter(e);
+    }
+
+    // TODO: update state of mind
+    for (final node in exitNodes.toSet()) {
+      value.remove(node);
+    }
+
+    for (final node in enterNodes.toSet()) {
+      value.add(node);
+    }
+
+    return value;
+  }
 }
